6fd12d8b2613e17f264c53706c3deb2d72f86eb3
mallocaholic <jrmarcondesb@gmail.com>
2023-06-24 19:33:06 -0300

    mudamo foi tudo boy
    Co-authored-by: Maria Eduarda Veras <eduardaveras@users.noreply.github.com>

---
+++ board.py
@@ -0,0 +1,113 @@
+import random
+import datetime
+import math
+
+class Board:
+    def __init__(self, size, dna=[]):
+
+        if dna == []:
+            dna = [random.randrange(0, 2) for i in range(size * (len(bin(size - 1)) - 2))]
+
+        # dna to string
+        self.dna = ''.join(map(str, dna))
+        
+        self.size = size
+        self.fitness = self.get_fitness()
+
+    def __str__(self):
+        return self.dna
+
+    def show(self):
+        board = self.get_board()
+        b = [self.size - 1 - i for i in board]
+
+        for i in range(0, self.size):
+            print(self.size - i , end=' ')
+            for j in range(0, self.size):
+                if b[j] == i:
+                    print('Q', end=' ')
+                else:
+                    print('.', end=' ')
+            print()
+
+        print(' ', end=' ')
+        for i in range(1, self.size + 1):
+            print(i, end=' ')
+
+        print()
+
+    def get_board(self):
+        # transform the board list to a binary string
+        binary = ''
+        for i in range(self.size):
+            # binary string board to list
+            board = []
+            s = len(bin(self.size - 1)) - 2
+
+            for k in range(0, len(self.dna), s):
+                board.append(int(self.dna[k:k+s], 2))
+            
+            return board
+    
+    def get_fitness(self):
+        board = self.get_board()
+        max_fitness = (self.size-1)*(self.size)/2
+        horizontal_collisions = sum([board.count(queen)-1 for queen in board])/2
+        diagonal_collisions = 0
+
+        n = len(board)
+        left_diagonal = [0] * 2*n
+        right_diagonal = [0] * 2*n
+        for i in range(n):
+            left_diagonal[i + board[i] - 1] += 1
+            right_diagonal[len(board) - i + board[i] - 2] += 1
+
+        diagonal_collisions = 0
+        for i in range(2*n-1):
+            counter = 0
+            if left_diagonal[i] > 1:
+                counter += left_diagonal[i]-1
+            if right_diagonal[i] > 1:
+                counter += right_diagonal[i]-1
+            diagonal_collisions += counter
+        
+        return int(max_fitness - (horizontal_collisions + diagonal_collisions))
+
+
+def binary_to_board(binary, size):
+        # binary string board to list
+        board = []
+        for k in range(0, len(binary), size):
+            board.append(int(binary[k:k+size], 2))
+        
+        return board
+    
+
+def board_to_binary(board, size):
+    # transform the board list to a binary string
+    binary = ''
+    # 0 até size-1 
+    for i in range(size):
+        num_bin = bin(board[i])[2:]
+        bin_size = len(bin(size)) - 2  
+        for j in range(len(num_bin), bin_size, 1):
+            binary += '0'
+        binary += bin(board[i])[2:] 
+
+    return binary
+
+
+def new_board(size):
+    return Board(size)
+
+
+if __name__ == '__main__':
+    new_board = [5, 3, 6, 0, 7, 1, 4, 2]
+    new_board2 = [2, 0, 6, 4, 7, 1, 3, 5]
+    new_board3 = Board(len(new_board), board=new_board)
+
+    print(Board.boardTobinary(new_board3))
+    # board = Board(len(new_board2), board=new_board2)
+    # board.show()
+    # print(board.gene)
+    # print(board.get_fitness())
--- genetic.py
+++ genetic.py
@@ -1,62 +1,112 @@
 import random
-import datetime
+from board import new_board, Board
+"""
 
-class Board:
-    def __init__(self, size, board=[]):
-        self.board = board
+Primeira parte:
+Representação (genótipo): string de bits
+Recombinação: “cut-and-crossfill” crossover
+Probabilidade de Recombinação: 90%
+Mutação: troca de genes
+Probabilidade de Mutação: 40%
+Seleção de pais: ranking - Melhor de 2 de 5 escolhidos aleatoriamente
+Seleção de sobreviventes: substituição do pior
+Tamanho da população: 100
+Número de filhos gerados: 2
+Inicialização: aleatória
+Condição de término: Encontrar a solução, ou 10.000 avaliações de fitness
+Fitness?
 
-        if self.board == []:
-            self.board = [random.randint(0, size-1) for i in range(size)]
 
-        self.size = size
+"""
+GENESET = "01"
+RECOMBINATION_PROBABILITY = 0.9
+MUTATION_PROBABILITY = 0.4
+POPULATION_SIZE = 100
+CHILDREN_NUMBER = 2
 
-    def show(self):
-        b = [self.size - 1 - i for i in self.board]
+class Genetic:
+    """ 
 
-        for i in range(0, self.size):
-            print(self.size - i , end=' ')
-            for j in range(0, self.size):
-                if b[j] == i:
-                    print('Q', end=' ')
-                else:
-                    print('.', end=' ')
-            print()
+    """
+    def __init__(self, dna_size, new_indiv_func, population_size=100, 
+                 gene_set="01", recombination_probability=0.9, 
+                 mutation_probability=0.4, children_number=2):
 
-        print(' ', end=' ')
-        for i in range(1, self.size + 1):
-            print(i, end=' ')
 
-        print()
+        self.dna_size = dna_size
+        self.population_size = population_size
+        self.geneset = gene_set
+        self.recombination_probability = recombination_probability
+        self.mutation_probability = mutation_probability
+        self.children_number = children_number
+        self.new_indiv_func = new_indiv_func
+
+        self.population = self.init_population()
+    # def run(self):
+    #     self.population = self.init_population(new_board)
+        # pass
     
-    def get_max_fitness(self):
-        return (self.size-1)*(self.size)/2
-
-    def get_fitness(self):
-        horizontal_collisions = sum([self.board.count(queen)-1 for queen in self.board])/2
-        diagonal_collisions = 0
-
-        n = len(self.board)
-        left_diagonal = [0] * 2*n
-        right_diagonal = [0] * 2*n
-        for i in range(n):
-            left_diagonal[i + self.board[i] - 1] += 1
-            right_diagonal[len(self.board) - i + self.board[i] - 2] += 1
-
-        diagonal_collisions = 0
-        for i in range(2*n-1):
-            counter = 0
-            if left_diagonal[i] > 1:
-                counter += left_diagonal[i]-1
-            if right_diagonal[i] > 1:
-                counter += right_diagonal[i]-1
-            diagonal_collisions += counter
+    def init_population(self):
+        population = []  
+        for i in range(self.population_size):
+            population.append(self.new_indiv_func(self.dna_size))
         
-        return int(self.get_max_fitness() - (horizontal_collisions + diagonal_collisions))
+        return population
+
+    def crossover_cutandfill(parent1, parent2):
+        cut = random.randint(0, len(parent1))
+        child1 = parent1[:cut] + parent2[cut:]
+        child2 = parent2[:cut] + parent1[cut:]
+        return child1, child2
+
+
+    def single_mutation(indiv, geneSet):
+        index = random.randrange(0, len(indiv))
+        indiv_mutated = list(indiv)
+        new_gene, alternate = random.sample(geneSet, 2)
+        indiv_mutated[index] = alternate if new_gene == indiv_mutated[index] else new_gene
+        return ''.join(indiv_mutated)   
+
+    def double_mutation(indiv):
+        indices= random.sample(range(len(indiv)), 2)
+        indiv_mutated = list(indiv)
+        temp = indiv_mutated[indices[0]]
+        indiv_mutated[indices[0]] = indiv_mutated[indices[1]]
+        indiv_mutated[indices[1]] = temp
+        return ''.join(indiv_mutated)
+
+    def parent_tournament(self, population, choices_size):
+        list_parents = random.sample(population, choices_size)
+        best_parents = sorted(list_parents, key=lambda indiv: indiv.fitness, reverse=True)
+
+        return best_parents
+            
+
+
+    def get_best(get_fitness, target_len, optimal_fitness, geneSet, display, mutate_function):
+        if not optimal_fitness > best_parent.Fitness:
+            return best_parent
+        
+        while True:
+            child = mutate_function(best_parent, geneSet, get_fitness)
+            if not child.Fitness > best_parent.Fitness:
+                continue
+            display(child)
+            
+            if not optimal_fitness > child.Fitness:
+                return child
+            best_parent = child
 
 
 if __name__ == '__main__':
-    new_board = [5, 3, 6, 0, 7, 1, 4, 2]
-    new_board2 = [2, 0, 6, 4, 7, 1, 3, 5]
-    board = Board(len(new_board2), board=new_board2)
-    board.show()
-    print(board.get_fitness())
+    g = Genetic(8, new_board, population_size=3)
+    for indiv in g.population:
+        # indiv.show()
+        print(indiv.fitness)
+
+    print()
+    for i in sorted(g.population, key=lambda indiv: indiv.fitness, reverse=True):
+        print(i.fitness)
+
+    
+
